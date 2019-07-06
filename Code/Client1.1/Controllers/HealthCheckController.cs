fc2e31d30b1e5e82de48af8e4584e1ac57ca94ad
余 承浩 <chenghaogegelove@163.com>
2019-07-06 09:29:21 +0800

    初次提交

---
+++ Code/Client1.1/Controllers/HealthCheckController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HealthCheckController: ControllerBase
+    {
+        [HttpGet]
+        public void Get()
+        {
+        }
+    }
+}
